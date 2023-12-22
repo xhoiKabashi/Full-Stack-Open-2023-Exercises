9176ba61ce5e5278ad762791240b5448dc31e75b
Xhokabash <xhokabash@gmail.com>
2023-12-23 00:17:28 +0100

    add database on Render

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const PhoneBook = require("./modules/phoneBook");
-const mongoose = require("mongoose");
+
 
 const app = express();
 
